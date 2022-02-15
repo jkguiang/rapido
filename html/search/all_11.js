d45ea5e40fd5b98ec4f380cec5047ad91923020c
jkguiang <jkguiang@users.noreply.github.com>
2022-02-15 02:32:53 +0000

    automated docs update

--- html/search/all_11.js
+++ html/search/all_11.js
@@ -3,6 +3,7 @@ var searchData=
   ['write_0',['write',['../classHist1D.html#ae45c2073b4c8f25a10ac534012feb22b',1,'Hist1D::write()'],['../classHist2D.html#aea99923e54c6b1af26c2b399cf648231',1,'Hist2D::write()']]],
   ['writecsv_1',['writeCSV',['../classCutflow.html#a43e58326fbce5306e2b3d080c6cd85f0',1,'Cutflow']]],
   ['writehists_2',['writeHists',['../classHistflow.html#a06a2e1683cdc7ac2731b4608abe15eb7',1,'Histflow']]],
-  ['writerow_3',['writeRow',['../classUtilities_1_1CSVFile.html#a592fc96c51382efb35c6708acbfa69c2',1,'Utilities::CSVFile']]],
-  ['writetfile_4',['writeTFile',['../classArbol.html#ade7d835bbe383a70006e86785ae5672c',1,'Arbol']]]
+  ['writemermaid_3',['writeMermaid',['../classCutflow.html#acd23a4f8f0fef9a0ce8714bd901d23fb',1,'Cutflow']]],
+  ['writerow_4',['writeRow',['../classUtilities_1_1CSVFile.html#a592fc96c51382efb35c6708acbfa69c2',1,'Utilities::CSVFile']]],
+  ['writetfile_5',['writeTFile',['../classArbol.html#ade7d835bbe383a70006e86785ae5672c',1,'Arbol']]]
 ];
