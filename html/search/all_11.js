ee04a5e9402e01ae317c41ffd4dc6523547e6d04
jkguiang <jkguiang@users.noreply.github.com>
2022-03-09 02:26:56 +0000

    automated docs update

--- html/search/all_11.js
+++ html/search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['write_0',['write',['../classHist1D.html#ae45c2073b4c8f25a10ac534012feb22b',1,'Hist1D::write()'],['../classHist2D.html#aea99923e54c6b1af26c2b399cf648231',1,'Hist2D::write()']]],
+  ['write_0',['write',['../classCutflow.html#a42bd2909f9e32bb12e7dbd717248a253',1,'Cutflow::write()'],['../classHist1D.html#ae45c2073b4c8f25a10ac534012feb22b',1,'Hist1D::write()'],['../classHist2D.html#aea99923e54c6b1af26c2b399cf648231',1,'Hist2D::write()']]],
   ['writecsv_1',['writeCSV',['../classCutflow.html#a43e58326fbce5306e2b3d080c6cd85f0',1,'Cutflow']]],
   ['writehists_2',['writeHists',['../classHistflow.html#a06a2e1683cdc7ac2731b4608abe15eb7',1,'Histflow']]],
   ['writemermaid_3',['writeMermaid',['../classCutflow.html#acd23a4f8f0fef9a0ce8714bd901d23fb',1,'Cutflow']]],
--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -5,10 +5,13 @@ var searchData=
   ['recursivefindterminus_2',['recursiveFindTerminus',['../classCutflow.html#a6949f1d9e7d74c6ab8cf0c6f28ac3701',1,'Cutflow']]],
   ['recursiveprint_3',['recursivePrint',['../classCutflow.html#a8d49242a5ff302d9654f135f7e478333',1,'Cutflow']]],
   ['recursivesearchprogeny_4',['recursiveSearchProgeny',['../classCutflow.html#ab90ee0d1a00b9859021afe179eac7d93',1,'Cutflow']]],
-  ['resetbranches_5',['resetBranches',['../classArbol.html#a4c2184d378ab21a9627a57098291a7c4',1,'Arbol']]],
-  ['resetval_6',['resetVal',['../classUtilities_1_1Variables.html#a0b54d767199c068a1324ef138eca433e',1,'Utilities::Variables']]],
-  ['resetvalue_7',['resetValue',['../classUtilities_1_1Variable.html#acaff38dda27c6f428bbb232782e0f29b',1,'Utilities::Variable']]],
-  ['resetvars_8',['resetVars',['../classUtilities_1_1Variables.html#a50c695d86e58e227049cd8589c7cec98',1,'Utilities::Variables']]],
-  ['run_9',['run',['../classCutflow.html#a8fc98b9c006b80db96576c8bdcf6a0a8',1,'Cutflow::run()'],['../classHistflow.html#a675b897276d33e146d1c2707023f1edd',1,'Histflow::run()'],['../classLooper.html#a8b259a7ad0c2aa45ae49d1ec08d37af0',1,'Looper::run()']]],
-  ['rununtil_10',['runUntil',['../classCutflow.html#a3b5a6dc6e9490037d190eca691295859',1,'Cutflow']]]
+  ['recursivewrite_5',['recursiveWrite',['../classCutflow.html#a4f7285409e90c76524ab0148e6fa0aa9',1,'Cutflow']]],
+  ['recursivewritecsv_6',['recursiveWriteCSV',['../classCutflow.html#a398daefa870d9f58381047324a880701',1,'Cutflow']]],
+  ['recursivewritemermaid_7',['recursiveWriteMermaid',['../classCutflow.html#a212a44164cbf1f1608d1b55567890ef7',1,'Cutflow']]],
+  ['resetbranches_8',['resetBranches',['../classArbol.html#a4c2184d378ab21a9627a57098291a7c4',1,'Arbol']]],
+  ['resetval_9',['resetVal',['../classUtilities_1_1Variables.html#a0b54d767199c068a1324ef138eca433e',1,'Utilities::Variables']]],
+  ['resetvalue_10',['resetValue',['../classUtilities_1_1Variable.html#acaff38dda27c6f428bbb232782e0f29b',1,'Utilities::Variable']]],
+  ['resetvars_11',['resetVars',['../classUtilities_1_1Variables.html#a50c695d86e58e227049cd8589c7cec98',1,'Utilities::Variables']]],
+  ['run_12',['run',['../classCutflow.html#a8fc98b9c006b80db96576c8bdcf6a0a8',1,'Cutflow::run()'],['../classHistflow.html#a675b897276d33e146d1c2707023f1edd',1,'Histflow::run()'],['../classLooper.html#a8b259a7ad0c2aa45ae49d1ec08d37af0',1,'Looper::run()']]],
+  ['rununtil_13',['runUntil',['../classCutflow.html#a3b5a6dc6e9490037d190eca691295859',1,'Cutflow']]]
 ];
