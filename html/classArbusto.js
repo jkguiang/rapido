42daa918be1b253e74bfbc2ad472dad071870781
jkguiang <jkguiang@users.noreply.github.com>
2022-08-11 00:40:45 +0000

    automated docs update

--- html/classArbusto.js
+++ html/classArbusto.js
@@ -3,6 +3,8 @@ var classArbusto =
     [ "Arbusto", "classArbusto.html#a74d2da33962ce8ab5386e690146dc16d", null ],
     [ "Arbusto", "classArbusto.html#a61dd706e32460f7d695892b33a66599f", null ],
     [ "~Arbusto", "classArbusto.html#ac0e7dc4a5a89df51fd5ac8a7b25f4cc3", null ],
+    [ "fill", "classArbusto.html#ad3e7b018a8b5a761bd7f3790a776337f", null ],
     [ "init", "classArbusto.html#a0b16917f2f868d502294408741eaac67", null ],
-    [ "keep_branch_names", "classArbusto.html#ad3dba0cbd9747912ba0b978f3b0ca60f", null ]
+    [ "keep_branch_names", "classArbusto.html#ad3dba0cbd9747912ba0b978f3b0ca60f", null ],
+    [ "orig_ttree", "classArbusto.html#a1d2fa85227f816ddbe865750b4dafe71", null ]
 ];
