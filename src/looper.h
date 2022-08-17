d31d71bf031cda7faa9a2fe71c325ba58b190da0
Jonathan Guiang <jkguiang@gmail.com>
2022-08-17 10:06:32 -0700

    moved HEPCLI to its on .h/.cc files, added HEPCLI constructors

--- src/looper.h
+++ src/looper.h
@@ -2,11 +2,6 @@
 #define LOOPER_H
 
 #include <functional>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <stdlib.h>
-#include <getopt.h>
 
 #include "TString.h"
 #include "TChain.h"
@@ -14,59 +9,6 @@
 #include "TTree.h"
 #include "TTreeCache.h"
 #include "TTreeCacheUnzip.h"
-
-/**
- * Object for handling HEP CLI input (wraps getopt functionality)
- */
-class HEPCLI
-{
-private:
-    /**
-     * Display help text
-     * @return none
-     */
-    void printHelp();
-    /**
-     * Parse command line
-     * @param argc argument count
-     * @param argv argument vector
-     * @return none
-     */
-    void parse(int argc, char** argv);
-public:
-    /** Verbosity flag */
-    bool verbose;
-    /** Name of TTree in input ROOT file(s) */
-    std::string input_ttree;
-    /** Target directory for output file(s) */
-    std::string output_dir;
-    /** Short name for output file(s) */
-    std::string output_name;
-    /** Data (as opposed to Monte Carlo) flag */
-    bool is_data;
-    /** Signal (as opposed to background) flag */
-    bool is_signal;
-    /** Debug flag */
-    bool debug;
-    /** Global event weight */
-    float scale_factor;
-    /** ROOT TChain with input files */
-    TChain* input_tchain;
-
-    /**
-     * HEPCLI object constructor
-     * @return none
-     */
-    HEPCLI();
-
-    /**
-     * HEPCLI object overload constructor
-     * @param argc argument count
-     * @param argv argument vector
-     * @return none
-     */
-    HEPCLI(int argc, char** argv);
-};
 
 /**
  * Object to handle looping over ROOT files
@@ -94,6 +36,12 @@ public:
      * @return none
      */
     Looper(TChain* new_tchain);
+    /**
+     * Looper object constructor
+     * @param cli HEPCLI object
+     * @return none
+     */
+    Looper(HEPCLI& cli);
     /**
      * Looper object destructor
      * @return none
